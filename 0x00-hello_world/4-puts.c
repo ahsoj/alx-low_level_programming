17ab61c737c2d43950fd710a5315c29525016b84
eyasu chaka <eyasuchaka259@gmail.com>
2022-06-02 06:56:31 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\""programming is like building a multilingual puzzle");
 	return (0);
 }
