77698673e6b641714e6e74eaca4dc21a9d470fd3
eyasu chaka <eyasuchaka259@gmail.com>
2022-06-02 07:13:30 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Retrun: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
