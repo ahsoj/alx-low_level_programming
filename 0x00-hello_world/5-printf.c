c1c04a8a77fc2974e4d4614e7b938909bf9f96c3
eyasu chaka <eyasuchaka259@gmail.com>
2022-06-02 07:08:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * maic - Entry point
+ *
+ * Retrun: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
