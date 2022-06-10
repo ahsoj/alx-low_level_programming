a16f3fc0e97d9a11f65843818986f8131f82e58d
eyasu chaka <eyasuchaka259@gmail.com>
2022-06-10 10:44:12 -0700

    1 task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	int i;
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-	while (i < 10)
-	{
-		puutcher(i);
-	}
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+while (i < 10)
+{
+	puutcher(i);
+}
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
 	
