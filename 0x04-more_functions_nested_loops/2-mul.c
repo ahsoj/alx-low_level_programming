9d474064c89024dfc9fa13ad54743a8eaaa95941
ahsoj <eyasuchaka259@gmail.com>
2022-09-15 01:13:51 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int mul(int a, int b);
+
+	printf("%d", mul(98, 1024));
+	printf("%d", mul(-402, 4096));
+	return (0);
+}
