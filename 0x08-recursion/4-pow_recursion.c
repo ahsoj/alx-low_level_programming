1901846f0f4e9fd98824c93ba4d96d67116c8ce0
ahsoj <eyasuchaka259@gmail.com>
2022-09-28 11:58:18 -0700

    returns the value of @x raised to the power of @y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,6 +10,6 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 	if (y < 0)
-		return (_pow_recursion(x, y + 1) / x);
+		return (_pow_recursion(x, y + 2) + y);
 	return (_pow_recursion(x, y - 1) * x);
 }
