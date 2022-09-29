49adf7e28677803d9a23959bf90185a1f6e76953
ahsoj <eyasuchaka259@gmail.com>
2022-09-29 11:51:22 -0700

    is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,14 +4,19 @@
  */
 int is_prime_number(int n)
 {
+	int i = 2;
+
 	if (n < 2)
+	{
 		return 0;
-	for (int i = 2;i * i <= n; i++)
+	}
+	while(i * i <= n)
 	{
 		if (n % i == 0)
 		{
 			return 0;
 		}
+		i++;
 	}
 	return 1;
 }
