94c6cf451bb3be28a4b1a1784c498963053cb178
ahsoj <eyasuchaka259@gmail.com>
2022-10-11 04:22:45 -0700

    head

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 
+
+/**                                                                                                                          
+ * struct dog - structure definition of a dog                                                                                
+ * @name: character string                                                                                                   
+ * @age: integer                                                                                                             
+ * @owner: character string                                                                                                  
+ */  
+
+
 struct dog                                                                                                                   
-{                                                                                                                            
+{	
         char *name;                                                                                                          
         float age;                                                                                                           
         char *owner;                                                                                                         
