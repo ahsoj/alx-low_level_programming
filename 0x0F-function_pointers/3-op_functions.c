2937697ac0e798899a17cbf6547523b5830c7f63
ahsoj <eyasuchaka259@gmail.com>
2022-10-12 09:00:02 -0700

    task-4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+/**
+ * op_add - calculate additions of @a and @b.
+ * @a: integer
+ * @b: integer
+ */
+int op_add(int a, int b)
+{
+	return (a+b);
+}
+/**
+ * op_sub - calculate subtractions of @a and @b
+ * @a: integer
+ * @b: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a-b);
+}
+/**
+ * op_mul - calculate multiplacation operator of @a and @b.
+ * @a: integer
+ * @b: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a*b);
+}
+/**
+ * op_div - calculate division operation of @a and @b.
+ * @: integer
+ * @b: integer
+ */
+int op_div(int a, int b)
+{
+	return (a/b);
+}
+/**
+ * op_mod - calculate remainder of division @ and @b.
+ * @: integer
+ * @b: integer
+ */
+int op_mod(int a, int b)
+{
+	return (a%b);
+}
