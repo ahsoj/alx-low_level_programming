06de1237807e15e0d31f4a0d43007a2af40d5cab
ahsoj <eyasuchaka259@gmail.com>
2022-10-20 10:46:34 -0700

    task-3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,7 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
+ * @head: head of the linked list
  * @str: string to store in the list
  *
  * Return: address to store in the head
