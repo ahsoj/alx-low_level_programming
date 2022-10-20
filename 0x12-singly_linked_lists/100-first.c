f0bd60413ac31facd32d4c6e0ab15ee1964c3c46
ahsoj <eyasuchaka259@gmail.com>
2022-10-20 10:30:00 -0700

    advanced task-100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
  * bmain - function excuted before main
  *
  * Return: no return
  */
 
-void __attribute__ ((constructor)) bmain()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n "); 
