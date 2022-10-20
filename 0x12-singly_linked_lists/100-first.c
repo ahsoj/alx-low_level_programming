43f589a2ddaba3dd9bf96635c265a3ffda94377b
ahsoj <eyasuchaka259@gmail.com>
2022-10-20 10:38:14 -0700

    advanced task-100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return: no return
  */
 
-void __attribute__ ((constructor)) bmain();
+void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n "); 
