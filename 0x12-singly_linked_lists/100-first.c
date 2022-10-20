de3614a2156d0aaac9c3207abbf0144cbce05f24
ahsoj <eyasuchaka259@gmail.com>
2022-10-20 10:08:32 -0700

    advanced task-100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,5 @@
 void __attribute__ ((constructor)) bmain()
 {
 	printf("You'r beat! and yet, youmust allow");
-	printf(",\nI bore my house upon my back!\n");
-	printf("(A tortoise, having pretty good sense of a hare's nature, challe		nges one to a race.)\n");
+	printf(",\nI bore my house upon my back!\n"); 
 }
