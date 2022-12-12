eb25d349458e1fa3ec4ee423a641f75aadaf0f9a
ahsoj <eyasuchaka259@gmail.com>
2022-12-12 10:21:03 -0800

    Dynamic_libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -11,10 +12,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int mod(int a, int b);
 
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
