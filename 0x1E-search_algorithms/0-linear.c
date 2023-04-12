b9d24bd6c9ecb571c576cfc8c9ae44887af3cb20
eyasu chaka <daa282386@gmail.com>
2023-04-12 02:02:49 -0700

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,16 +9,18 @@
   * Return: NULL if the value isn't present, else -1
   */
 
-int linear_search(int *array, size_t size, int value) {
-    size_t i;
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		return (i);
+	}
 
-    for (i = 0; i < size; i++) {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	return (-1);
 }
