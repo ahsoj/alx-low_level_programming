0c1e95460a2b217b89419bf6c0b03cc70702bbef
eyasu chaka <daa282386@gmail.com>
2023-04-12 02:12:53 -0700

    1-binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,21 +9,29 @@
   * Return: NULL if value is not present else -1
   */
 
-int binary_search(int *array, size_t size, int value) {
-    
-    size_t i, lt, rt;
-    if (array == NULL) return (-1);
+int binary_search(int *array, size_t size, int value)
+{
 
-    for (lt = 0, rt = size - 1; rt >= lt;) {
-        printf("Searching in array: ");
-        for (i = lt; i < rt; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[i]);
+	size_t i, lt, rt;
 
-        i = lt + (rt - lt) / 2;
-        if (array[i] ==value) return (i);
-        if (array[i] > value) rt = i - 1;
-        else lt = i + 1;
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+
+	for (lt = 0, rt = size - 1; rt >= lt;)
+	{
+		printf("Searching in array: ");
+			for (i = lt; i < rt; i++)
+				printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = lt + (rt - lt) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			rt = i - 1;
+		else
+			lt = i + 1;
+	}
+
+	return (-1);
 }
