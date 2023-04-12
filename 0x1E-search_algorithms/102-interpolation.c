817b52847e7fbbc70989c752a58df36074eb99af
eyasu chaka <daa282386@gmail.com>
2023-04-12 03:47:04 -0700

    102-interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,23 +12,34 @@
   *
   */
 
-int interpolation_search(int *array, size_t size, int value) {
+int interpolation_search(int *array, size_t size, int value)
+{
 
-    size_t i, lt, rt;
+	size_t i, lt, rt;
 
-    if (array == NULL) return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (lt = 0, rt = size - 1; rt >= lt;) {
-        i = lt + (((double)(rt - lt) / (array[rt] - array[lt])) * (value - array[lt]));
-        if (i < size) printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        else {
-            printf("Value checked array[%ld] is out of range\n", i);
-            break;
-        }
-        if (array[i] == value) return (i);
-        if (array[i] > value) rt = i - 1;
-        else lt = i + 1;
-    }
+	for (lt = 0, rt = size - 1; rt >= lt;)
+	{
+		i = lt + (
+			((double)(rt - lt) / (array[rt] - array[lt])
+			) * (value - array[lt])
+			);
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+	if (array[i] == value)
+		return (i);
+	if (array[i] > value)
+		rt = i - 1;
+	else
+		lt = i + 1;
+	}
 
-    return (-1);
+	return (-1);
 }
